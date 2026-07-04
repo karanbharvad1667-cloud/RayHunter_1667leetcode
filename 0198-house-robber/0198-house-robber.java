712978686213a11eb70df94b4872894039d92b4b
RayHunter_1667 <karanbharvad1667@gmail.com>
2026-07-05 00:49:44 +0530

    Time: 0 ms (100%), Space: 42.6 MB (74.13%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,21 +1,37 @@
 class Solution {
-    static int[]dp;
+    
     public int rob(int[] arr) {
         int n=arr.length;
-        dp=new int[n];
-        Arrays.fill(dp,-1);
-       return  loot(0,arr);
-    }
-    public int loot(int i,int[] arr) {
-        if(i>=arr.length) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int pick=arr[i]+loot(i+2,arr);
-        int skip=loot(i+1,arr);
-        int ans=Math.max(pick,skip);
-        dp[i]=ans;
-        return ans;
+        int[]dp=new int[n];
+        dp[0]=arr[0];
+        if(n>1) dp[1]=Math.max(arr[0],arr[1]);
+        for(int i=2;i<n;i++){
+            dp[i]=Math.max(arr[i]+dp[i-2],dp[i-1]);
+        }
+        return dp[n-1];
     }
 }
+// .... recursion  +memoization
+// class Solution {
+//     static int[]dp;
+//     public int rob(int[] arr) {
+//         int n=arr.length;
+//         dp=new int[n];
+//         Arrays.fill(dp,-1);
+//         return  loot(0,arr);
+//     }
+//     public int loot(int i,int[] arr) {
+//         if(i>=arr.length) return 0;
+//         if(dp[i]!=-1) return dp[i];
+//         int pick=arr[i]+loot(i+2,arr);
+//         int skip=loot(i+1,arr);
+//         int ans=Math.max(pick,skip);
+//         dp[i]=ans;
+//         return ans;
+//         //return dp[i]=Math.max(pick,skip);
+//     }
+// }
+
 // this is the recursion code 
 // class Solution {
 //     public int rob(int[] arr) {
