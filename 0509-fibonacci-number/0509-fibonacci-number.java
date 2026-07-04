2dee7598b39c45165aa67068f2f5a1153e9567c6
RayHunter_1667 <karanbharvad1667@gmail.com>
2026-07-05 00:23:45 +0530

    Time: 0 ms (100%), Space: 41.9 MB (69.58%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,14 +1,26 @@
 class Solution {
-    static int[] dp;
     public int fib(int n) {
-        dp = new int[n + 1];
-        return dp(n);
-    }
-    public int dp(int n) {
+        int[] dp = new int[n + 1];
         if (n <= 1) return n;
-        if (dp[n] != 0) return dp[n];
-        int ans = dp(n - 1) + dp(n - 2);
-        dp[n] = ans;
-        return ans;
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 }
+// class Solution {
+//     static int[] dp;
+//     public int fib(int n) {
+//         dp = new int[n + 1];
+//         return dp(n);
+//     }
+//     public int dp(int n) {
+//         if (n <= 1) return n;
+//         if (dp[n] != 0) return dp[n];//extra
+//         int ans = dp(n - 1) + dp(n - 2);
+//         dp[n] = ans;//extra
+//         return ans;
+//     }
+// }
