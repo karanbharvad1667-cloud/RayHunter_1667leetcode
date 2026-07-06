25808fb56c5b9c15d51c639326e54b0b7874f0e5
RayHunter_1667 <karanbharvad1667@gmail.com>
2026-07-06 07:16:43 +0530

    Time: 0 ms (100%), Space: 41.6 MB (96.69%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,15 +1,32 @@
 class Solution {
+    //3. tabulation tc o(n) as o(n)
     public int fib(int n) {
-        int[] dp = new int[n + 1];
+        int[] dp = new int[3];
         if (n <= 1) return n;
-        dp[0]=0;
-        dp[1]=1;
+        dp[0]=0;dp[1]=1;  
         for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            dp[2]=dp[1]+dp[0];
+            dp[0]=dp[1];
+            dp[1]=dp[2];
         }
-        return dp[n];
+        return dp[2];
     }
 }
+// class Solution {
+//     //3. tabulation tc o(n) as o(n)
+//     public int fib(int n) {
+//         int[] dp = new int[n + 1];
+//         if (n <= 1) return n;
+//         dp[0]=0;dp[1]=1;  
+//         for(int i=2;i<=n;i++){
+//             dp[i]=dp[i-1]+dp[i-2];
+//         }
+//         return dp[n];
+//     }
+// }
+//1. in simple recursion O(2^n)
+//2. recursion +memoization
+ // tc=o(n) as=>O(n)(dp array) + O(n) (stack) = O(n)
 // class Solution {
 //     static int[] dp;
 //     public int fib(int n) {
