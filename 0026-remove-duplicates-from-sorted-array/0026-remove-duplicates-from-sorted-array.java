603f4544e439a803815625a546e205ffc8662f33
RayHunter_1667 <karanbharvad1667@gmail.com>
2026-07-11 10:51:13 +0530

    Time: 0 ms (100%), Space: 46.9 MB (24.28%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,12 +1,24 @@
 class Solution {
     public int removeDuplicates(int[] arr) {
-        int idx=1;
-        for(int i=1;i<arr.length;i++){
-            if(arr[i]!=arr[idx-1]){
-                arr[idx]=arr[i];
-                idx++;
+        int slow=0;
+        for(int fast=1;fast<arr.length;fast++){
+            if(arr[fast]!=arr[slow]){
+                slow++;
+                arr[slow]=arr[fast];
             }
         }
-        return idx;
+        return slow+1;
     }
 }
+// class Solution {
+//     public int removeDuplicates(int[] arr) {
+//         int idx=1;
+//         for(int i=1;i<arr.length;i++){
+//             if(arr[i]!=arr[idx-1]){
+//                 arr[idx]=arr[i];
+//                 idx++;
+//             }
+//         }
+//         return idx;
+//     }
+// }
