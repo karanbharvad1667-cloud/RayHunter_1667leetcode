c46cf76e2fc739432e74085afbc7d7669d73a704
RayHunter_1667 <karanbharvad1667@gmail.com>
2026-07-14 22:36:11 +0530

    Time: 0 ms (100%), Space: 61 MB (93.47%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -2,7 +2,7 @@ class Solution {
     public void rotate(int[] arr, int k) {
         int n = arr.length;
         k = k % n;
-
+// this is for right rotation
         reverse(arr, 0, n - 1);
         reverse(arr, 0, k - 1);
         reverse(arr, k, n - 1);
