e844af62c23bc59e0e33289bcf25feb8a82baccd
RayHunter_1667 <karanbharvad1667@gmail.com>
2026-07-17 22:13:12 +0530

    Time: 0 ms (100%), Space: 44.7 MB (32.81%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,41 +1,27 @@
 class Solution {
-    public void nextPermutation(int[] nums) {
-        int n = nums.length;
-
+    public void nextPermutation(int[] arr) {
+        int n = arr.length;
         // Step 1: Find the pivot (first decreasing element from the right)
         int i = n - 2;
-        while (i >= 0 && nums[i] >= nums[i + 1]) {
-            i--;
-        }
-
+        while (i >= 0 && arr[i] >= arr[i + 1]) i--;
         // Step 2: If pivot exists, find the next greater element
         if (i >= 0) {
             int j = n - 1;
-            while (nums[j] <= nums[i]) {
-                j--;
-            }
-
+            while (arr[j] <= arr[i])  j--;
             // Swap pivot and next greater element
-            swap(nums, i, j);
+            swap(arr, i, j);
         }
-
         // Step 3: Reverse the suffix
-        reverse(nums, i + 1, n - 1);
+        reverse(arr, i + 1, n - 1);
     }
-
-    // Swap two elements
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
-
-    // Reverse the array from left to right
-    private void reverse(int[] nums, int left, int right) {
+    private void reverse(int[] arr, int left, int right) {
         while (left < right) {
-            swap(nums, left, right);
-            left++;
-            right--;
+            swap(arr, left++, right--);
         }
     }
 }
