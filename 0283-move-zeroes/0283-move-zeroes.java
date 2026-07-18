13655c49a8eeaab9973b25a7f3af8a7ae1283aaa
RayHunter_1667 <karanbharvad1667@gmail.com>
2026-07-18 22:55:09 +0530

    Time: 2 ms (92.15%), Space: 47.8 MB (57.29%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,13 +1,13 @@
 class Solution {
     public void moveZeroes(int[] arr){
-        int n=arr.length;
-       for(int i=0;i<n-1;i++){
-           for(int j=0;j<n-1;j++){
-            if(arr[j]==0){
-                swap(arr,j,j+1);
-            }
-           }
-       }
+    int n=arr.length;
+    int j=0;
+       for(int i=0;i<n;i++){
+          if(arr[i]!=0){
+            swap(arr,i,j);
+            j++;
+          }
+        } 
     }
     public void swap(int arr[],int i,int j){
         int temp=arr[i];
@@ -15,3 +15,18 @@ class Solution {
         arr[j]=temp;
     }
 }
+// class Solution {
+//     public void moveZeroes(int[] arr){
+//     int n=arr.length;
+//        for(int i=0;i<n-1;i++){
+//            for(int j=0;j<n-1;j++){
+//             if(arr[j]==0) swap(arr,j,j+1);
+//            }
+//         }
+//     }
+//     public void swap(int arr[],int i,int j){
+//         int temp=arr[i];
+//         arr[i]=arr[j];
+//         arr[j]=temp;
+//     }
+// }
