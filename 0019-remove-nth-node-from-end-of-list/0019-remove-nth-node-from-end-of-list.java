babb0c122007f3533b75ea7ead86fae35d408965
RayHunter_1667 <karanbharvad1667@gmail.com>
2026-07-23 11:12:51 +0530

    Time: 0 ms (100%), Space: 43.4 MB (58.92%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -1,18 +1,18 @@
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode temp=new ListNode(0);
-        temp.next=head;
-
-        ListNode slow=temp;
-        ListNode fast=temp;
-        for(int i=0;i<=n;i++){
+        ListNode slow=head;
+        ListNode temp=head;
+        ListNode fast=head;
+        int size=0;
+        while(temp!=null){
+            size++;
+            temp=temp.next;
+        }
+        if(size==n) return head.next;
+        for(int i=1;i<size-n;i++){
             fast=fast.next;
         }
-        while(fast!=null){
-            slow=slow.next;
-            fast=fast.next;
-        }
-        slow.next=slow.next.next;
-        return temp.next;
+        fast.next=fast.next.next;
+        return head;
     }
 }
