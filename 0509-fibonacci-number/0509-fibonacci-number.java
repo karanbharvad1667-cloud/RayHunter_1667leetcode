78b01bf2cd680b1d0c64c843d1222c0795ade67a
RayHunter_1667 <karanbharvad1667@gmail.com>
2026-07-26 23:03:39 +0530

    Time: 0 ms (100%), Space: 42.1 MB (52.47%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,49 +1,57 @@
+//4.tabulation tc o(n) as o(1) // tb with space optimization
+class Solution {   
+    public int fib(int n) {
+        int[] dp = new int[3];
+        if (n <= 1) return n;
+        dp[0]=0;dp[1]=1;  
+        for(int i=2;i<=n;i++){
+            dp[2]=dp[1]+dp[0];
+            dp[0]=dp[1];
+            dp[1]=dp[2];
+        }
+        return dp[2];
+    }
+}
+
+/*
+//3. tabulation tc o(n) as o(n)
 class Solution {
     public int fib(int n) {
+        int[] dp = new int[n + 1];
+        if (n <= 1) return n;
+        dp[0]=0;dp[1]=1;  
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+}*/
+
+
+/*2. recursion +memoization
+// tc=o(n) as=>O(n)(dp array) + O(n) (stack) = O(n)
+class Solution {
+    public int fib(int n) {
+        int []dp = new int[n + 1];
+        return dp(n,dp);
+    }
+    public int dp(int n,int []dp) {
+        if (n <= 1) return n;
+        if (dp[n] != 0) return dp[n];//extra
+        int ans = dp(n - 1) + dp(n - 2);
+        dp[n] = ans;//extra
+        return ans;
+    }
+}
+*/
+
+
+/*//1. in simple recursion O(2^n)
+    class Solution {
+        public int fib(int n) {
         if(n<=1) return n;
         return fib(n-1)+fib(n-2);
-    }  
-}
-// class Solution {
-//     //3. tabulation tc o(n) as o(1) // space optimization
-//     public int fib(int n) {
-//         int[] dp = new int[3];
-//         if (n <= 1) return n;
-//         dp[0]=0;dp[1]=1;  
-//         for(int i=2;i<=n;i++){
-//             dp[2]=dp[1]+dp[0];
-//             dp[0]=dp[1];
-//             dp[1]=dp[2];
-//         }
-//         return dp[2];
-//     }
-// }
-// class Solution {
-//     //3. tabulation tc o(n) as o(n)
-//     public int fib(int n) {
-//         int[] dp = new int[n + 1];
-//         if (n <= 1) return n;
-//         dp[0]=0;dp[1]=1;  
-//         for(int i=2;i<=n;i++){
-//             dp[i]=dp[i-1]+dp[i-2];
-//         }
-//         return dp[n];
-//     }
-// }
-//1. in simple recursion O(2^n)
-//2. recursion +memoization
- // tc=o(n) as=>O(n)(dp array) + O(n) (stack) = O(n)
-// class Solution {
-//     static int[] dp;
-//     public int fib(int n) {
-//         dp = new int[n + 1];
-//         return dp(n);
-//     }
-//     public int dp(int n) {
-//         if (n <= 1) return n;
-//         if (dp[n] != 0) return dp[n];//extra
-//         int ans = dp(n - 1) + dp(n - 2);
-//         dp[n] = ans;//extra
-//         return ans;
-//     }
-// }
+        }  
+    }
+*/
+
