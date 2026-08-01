b3d9e5b5df2ae8e8602c160bab72141795b41255
RayHunter_1667 <karanbharvad1667@gmail.com>
2026-08-01 22:53:13 +0530

    Time: 1 ms (100%), Space: 43.6 MB (76.43%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,29 +1,77 @@
 class Solution {
+
     public int myAtoi(String s) {
         int i = 0;
         int n = s.length();
 
-        // Step 1: skip leading spaces
-        while (i < n && s.charAt(i) == ' ')   i++;
+        // skip spaces
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
 
-        // Step 2: check sign
         int sign = 1;
+
+        // sign
         if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
-            if (s.charAt(i) == '-')  sign = -1;
+            if (s.charAt(i) == '-') sign = -1;
             i++;
         }
 
-        // Step 3: convert digits
-        long num = 0;
-        while (i < n && Character.isDigit(s.charAt(i))) {
-            num = num * 10 + (s.charAt(i) - '0');
-            // Step 4: handle overflow
-            if (sign == 1 && num > Integer.MAX_VALUE) return Integer.MAX_VALUE;
-            if (sign == -1 && -num < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+        long ans = solve(s, i, 0);
 
-            i++;
+        ans = ans * sign;
+
+        if (ans > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+        if (ans < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+
+        return (int) ans;
+    }
+
+    private long solve(String s, int i, long num) {
+
+        // base case
+        if (i >= s.length() || !Character.isDigit(s.charAt(i))) {
+            return num;
         }
 
-        return (int)(sign * num);
+        num = num * 10 + (s.charAt(i) - '0');
+
+        // overflow pruning
+        if (num > Integer.MAX_VALUE) {
+            return num;
+        }
+
+        return solve(s, i + 1, num);
     }
 }
+
+
+// class Solution {
+//     public int myAtoi(String s) {
+//         int i = 0;
+//         int n = s.length();
+
+//         // Step 1: skip leading spaces
+//         while (i < n && s.charAt(i) == ' ')   i++;
+
+//         // Step 2: check sign
+//         int sign = 1;
+//         if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+//             if (s.charAt(i) == '-')  sign = -1;
+//             i++;
+//         }
+
+//         // Step 3: convert digits
+//         long num = 0;
+//         while (i < n && Character.isDigit(s.charAt(i))) {
+//             num = num * 10 + (s.charAt(i) - '0');// ASCCI CONVERSION
+//             // Step 4: handle overflow
+//             if (sign == 1 && num > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+//             if (sign == -1 && -num < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+
+//             i++;
+//         }
+
+//         return (int)(sign * num);
+//     }
+// }
