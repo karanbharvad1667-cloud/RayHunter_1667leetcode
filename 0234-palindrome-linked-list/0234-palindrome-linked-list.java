acab5335ec0253e185800cecd6b9caf2d68ae703
RayHunter_1667 <karanbharvad1667@gmail.com>
2026-08-02 10:47:08 +0530

    Time: 4 ms (65.92%), Space: 94.6 MB (36.86%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,45 +1,54 @@
-
 class Solution {
-    //   public ListNode reverse(ListNode head){
-    //      if(head==null||head.next==null) return head;
-    //     ListNode c=head;
-    //     ListNode p=null;
-    //     ListNode f=null;
-    //     while(c!=null){
-    //        f=c.next;
-    //        c.next=p;
-    //        p=c;
-    //        c=f;
-    //     }
-    //     return p;
-    //   }
+
+    public ListNode reverse(ListNode head){
+        ListNode c = head;
+        ListNode p = null;
+
+        while(c != null){
+            ListNode f = c.next;
+            c.next = p;
+            p = c;
+            c = f;
+        }
+        return p;
+    }
+
     public boolean isPalindrome(ListNode head) {
-        // ListNode slow=head;
-        // ListNode fast=head;
-        // while(fast!=null){
-        //     slow=slow.next;
-        //     fast=fast.next.next;
-        // }
-        // ListNode head2=slow;
-        // slow.next=null;
-        // head2=reverse(head2);
-        // ListNode i=head;
-        // ListNode j=head2;
-        // while(j!=null){
-        //     if(i.val!=j.val) return false;
-        // }
-        // return true;
-    ListNode temp=head;
-        ArrayList<Integer>arr=new ArrayList<>();
-        while(temp!=null){
-            arr.add(temp.val);
-            temp=temp.next;
+
+        if(head == null || head.next == null) return true;
+
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+
+        // find middle
+        while(fast != null && fast.next != null){
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        int n=arr.size();
-        int i=0,j=n-1;
-        while(i<=j){
-            if(arr.get(i++)!=arr.get(j--)) return false;
+
+        // odd length: skip middle node
+        if(fast != null){
+            slow = slow.next;
         }
+
+        // split list
+        prev.next = null;
+
+        // reverse second half
+        ListNode head2 = reverse(slow);
+
+        // compare
+        ListNode i = head;
+        ListNode j = head2;
+
+        while(j != null){
+            if(i.val != j.val) return false;
+            i = i.next;
+            j = j.next;
+        }
+
         return true;
     }
 }
