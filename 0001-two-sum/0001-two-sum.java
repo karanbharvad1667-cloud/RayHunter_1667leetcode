dbd510fb5261daf764df5a678732b44379eaa929
RayHunter_1667 <karanbharvad1667@gmail.com>
2026-08-05 20:39:36 +0530

    Time: 6 ms (47.57%), Space: 46.9 MB (85.25%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,14 +1,33 @@
-// done 1
-class Solution { // time complexity =0(n)
-    public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    //int arr[]={ i, j };
-                    return new int[] {i,j};
-                }
-            }
-        }
-        return new int[] {}; // if no solution
+class Solution {
+    public int[] twoSum(int[] arr, int target) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+        map.put(arr[i],i);
+       }
+       int brr[]=new int[2];
+       for(int i=0;i<arr.length;i++){
+           int mising=target-arr[i];
+           if(map.containsKey(mising)&&map.get(mising)!=i){
+            brr[0]=i;
+            brr[1]=map.get(mising);
+           }
+       }
+       return brr;
     }
 }
+
+
+
+// class Solution { // time complexity =0(n^2)
+//     public int[] twoSum(int[] nums, int target) {
+//         for (int i = 0; i < nums.length; i++) {
+//             for (int j = i + 1; j < nums.length; j++) {
+//                 if (nums[i] + nums[j] == target) {
+//                     //int arr[]={ i, j };
+//                     return new int[] {i,j};
+//                 }
+//             }
+//         }
+//         return new int[] {}; // if no solution
+//     }
+// }
