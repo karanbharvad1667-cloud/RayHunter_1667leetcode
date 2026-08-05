bd6fb7cef3c1ec40ee08a81a5caeb928dc65b6e9
RayHunter_1667 <karanbharvad1667@gmail.com>
2026-08-05 21:44:01 +0530

    Time: 19 ms (12.69%), Space: 52.5 MB (85.67%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,20 +1,30 @@
 class Solution {
     public int majorityElement(int[] arr) {
-        int n=arr.length;
-       
-        int maxcount=-1;
-        int maxele=0;
-        for(int i=0;i<n;i++){ 
-            int count=0;
-            for(int j=i;j<n;j++){
-                if(arr[i]==arr[j]) count++;
-            }
-            if(count>maxcount){
-                maxcount=count;
-                maxele=arr[i];
-            }
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int n =arr.length;
+        for(int x:arr){
+            if(map.containsKey(x)) map.put(x,map.get(x)+1);
+            else map.put(x,1);
 
+            if(map.get(x)>n/2) return x;
         }
-       return maxele;
+        return -1;
     }
 }
+
+
+//tc=O(n^2)
+// class Solution {
+//     public int majorityElement(int[] arr) {
+//         int n=arr.length;
+//         int maxele=0;
+//         for(int i=0;i<n;i++){ 
+//             int count=0;
+//             for(int j=i;j<n;j++){
+//                 if(arr[i]==arr[j]) count++;
+//             }
+//             if(count>n/2) maxele=arr[i]; 
+//         }
+//        return maxele;
+//     }
+// }
