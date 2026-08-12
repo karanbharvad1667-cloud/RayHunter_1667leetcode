df321c3c2fb2b29c697f7e6b8789e38e8be24b9a
RayHunter_1667 <karanbharvad1667@gmail.com>
2026-08-12 08:47:17 +0530

    Time: 30 ms (73.93%), Space: 95.4 MB (66.21%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,24 +1,47 @@
 class Solution {
     public int longestConsecutive(int[] arr) {
-        Arrays.sort(arr);
-        int longest=1;
         int n=arr.length;
         if(n==0) return 0;
-        int pre=Integer.MIN_VALUE;
-        int count=0;
-        for(int i=0;i<n;i++){
-            if(arr[i]-1==pre){
-                count++;
-                pre=arr[i];
-            }else if(arr[i]!=pre){
-                count=1;
-                pre=arr[i];
-            }
+        int longest=0;
+        HashSet<Integer> set=new HashSet<>();
+        for(int ele:arr){
+            set.add(ele);
+        }
+        for(int ele:set){
+            if(!set.contains(ele-1)){
+                int count=0;
+                int x=ele;
+                while(set.contains(x)){
+                    count++;
+                    x=x+1;
+                }
             longest=Math.max(longest,count);
+            }
         }
         return longest;
     }
 }
+// class Solution {
+//     public int longestConsecutive(int[] arr) {
+//         Arrays.sort(arr);
+//         int longest=1;
+//         int n=arr.length;
+//         if(n==0) return 0;
+//         int pre=Integer.MIN_VALUE;
+//         int count=0;
+//         for(int i=0;i<n;i++){
+//             if(arr[i]-1==pre){
+//                 count++;
+//                 pre=arr[i];
+//             }else if(arr[i]!=pre){
+//                 count=1;
+//                 pre=arr[i];
+//             }
+//             longest=Math.max(longest,count);
+//         }
+//         return longest;
+//     }
+// }
 
 // brute time complexity=(n^2)
 // class Solution {
