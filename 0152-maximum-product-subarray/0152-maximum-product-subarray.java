ccd28976a497f6ddf1872ba9281882fddaab940e
RayHunter_1667 <karanbharvad1667@gmail.com>
2026-08-12 22:02:28 +0530

    Time: 2 ms (71.81%), Space: 47.3 MB (34.54%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,17 +1,39 @@
 class Solution {
     public int maxProduct(int[] arr) {
-        int n = arr.length;
         int max = arr[0];
+        int min = arr[0];
+        int ans = arr[0];
 
-        for (int i = 0; i < n; i++) {
-            int product = 1;
+        for (int i = 1; i < arr.length; i++) {
 
-            for (int j = i; j < n; j++) {
-                product *= arr[j];
-                max = Math.max(max, product);
+            // If current element is negative, swap max and min
+            if (arr[i] < 0) {
+                int temp = max;
+                max = min;
+                min = temp;
             }
+
+            max = Math.max(arr[i], max * arr[i]);
+            min = Math.min(arr[i], min * arr[i]);
+
+            ans = Math.max(ans, max);
         }
 
-        return max;
+        return ans;
     }
 }
+// class Solution {
+//     public int maxProduct(int[] arr) {
+//         int n = arr.length;
+//         int max = arr[0];
+//         for (int i = 0; i < n; i++) {
+//             int product = 1;
+
+//             for (int j = i; j < n; j++) {
+//                 product *= arr[j];
+//                 max = Math.max(max, product);
+//             }
+//         }
+//         return max;
+//     }
+// }
