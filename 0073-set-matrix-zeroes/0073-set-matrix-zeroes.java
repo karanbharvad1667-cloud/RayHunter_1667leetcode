179dff65a219ce5d519249d489bc064c49bec235
RayHunter_1667 <karanbharvad1667@gmail.com>
2026-08-13 07:07:08 +0530

    Time: 1 ms (97.43%), Space: 47.5 MB (62.7%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,30 +2,61 @@ class Solution {
     public void setZeroes(int[][] arr) {
         int n=arr.length;
         int m=arr[0].length;
+        int col[]=new int[m];
+        int row[]=new int[n];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(arr[i][j]==0){
-                    for(int k=0;k<n;k++){
-                        if(arr[k][j]!=0){
-
-                        arr[k][j]=-1000;
-                        }
-                    }
-                    for(int h=0;h<m;h++){
-                        if(arr[i][h]!=0){
-
-                        arr[i][h]=-1000;
-                        }
-                    }
+                    col[j]=1;
+                    row[i]=1;
                 }
             }
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(arr[i][j]==-1000){
+                if(col[j]==1||row[i]==1){
                     arr[i][j]=0;
                 }
             }
         }
     }
 }
+
+
+// class Solution {
+//     public void setZeroes(int[][] arr) {
+//         int n=arr.length;
+//         int m=arr[0].length;
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<m;j++){
+//                 if(arr[i][j]==0){
+//                    markrow(i,arr);
+//                    markcol(j,arr);
+//                 }
+//             }
+//         }   
+//      for(int i=0;i<n;i++){
+//             for(int j=0;j<m;j++){
+//                 if(arr[i][j]==-1000){
+//                     arr[i][j]=0;
+//                 }
+//             }
+//         }
+//     } 
+//     public void markrow(int i,int[][]arr){
+//         int m=arr[0].length;
+//         for(int j=0;j<m;j++){
+//             if(arr[i][j]!=0){
+//             arr[i][j]=-1000;
+//            }
+//         }
+//     }
+//     public void markcol(int j,int[][]arr){
+//         int n=arr.length;
+//         for(int i=0;i<n;i++){
+//             if(arr[i][j]!=0){
+//             arr[i][j]=-1000;
+//            }
+//         }
+//     }
+// }
