98f12948c973d74d17165c53f53b78473332b807
RayHunter_1667 <karanbharvad1667@gmail.com>
2026-08-18 09:18:34 +0530

    Time: 3 ms (72.7%), Space: 52 MB (86.14%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,20 +1,42 @@
+// optimal tc=(2n) space (1)
 class Solution {
     public List<Integer> majorityElement(int[] arr) {
-        int n=arr.length;
-        List<Integer> ans=new ArrayList<>();
+       List<Integer> ans=new ArrayList<>();
+       int n=arr.length;
+       int cnt1=0,cnt2=0;
+        int ele1=Integer.MIN_VALUE;
+        int ele2=Integer.MIN_VALUE;
         for(int i=0;i<n;i++){
-            if(ans.size()==0||ans.get(0)!=arr[i]){
-                int count=0;
-                for(int j=0;j<n;j++){
-                    if(arr[i]==arr[j]) count++;
-                }
-                if(count>n/3) ans.add(arr[i]);
+            if(cnt1==0&& arr[i]!=ele2){
+                cnt1=1;
+                ele1=arr[i];
+            }else if(cnt2==0 && arr[i]!=ele1){
+                cnt2=1;
+                ele2=arr[i];
+            }else if(arr[i]==ele1){
+             cnt1++;     
+            }else if(arr[i]==ele2){
+                cnt2++;
+            }else{
+                cnt1--;
+                cnt2--;
             }
-            if(ans.size()==2) break;
         }
-        return ans;
+       cnt1=0;
+       cnt2=0;
+       for(int i=0;i<n;i++){
+        if(arr[i]==ele1) cnt1++;
+        if(arr[i]==ele2) cnt2++;
+       }
+       if(cnt1>n/3) ans.add(ele1);
+       if(cnt2>n/3) ans.add(ele2);
+       ans.sort(null);;
+        
+       return ans;
     }
-} 
+}
+
+// better (n) sp=(n)
 // class Solution {
 //     public List<Integer> majorityElement(int[] arr) {
 //         int n=arr.length;
@@ -29,6 +51,25 @@ class Solution {
 //         // for(int key : map.keySet()){
 //         //    if(map.get(ele)==(n/3)+1) ans.add(ele);
 //         // }
+//         return ans;
+//     }
+// } 
+
+// // bruteforce tc=(n^2) space (1)
+// class Solution {
+//     public List<Integer> majorityElement(int[] arr) {
+//         int n=arr.length;
+//         List<Integer> ans=new ArrayList<>();
+//         for(int i=0;i<n;i++){
+//             if(ans.size()==0||ans.get(0)!=arr[i]){
+//                 int count=0;
+//                 for(int j=0;j<n;j++){
+//                     if(arr[i]==arr[j]) count++;
+//                 }
+//                 if(count>n/3) ans.add(arr[i]);
+//             }
+//             if(ans.size()==2) break;
+//         }
 //         return ans;
 //     }
 // } 
