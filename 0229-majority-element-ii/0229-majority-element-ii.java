6bf2406f2202a2c2b50e7bde2c84da4271a287be
RayHunter_1667 <karanbharvad1667@gmail.com>
2026-08-18 08:22:21 +0530

    Time: 19 ms (11.48%), Space: 52.1 MB (83.11%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -3,15 +3,15 @@ class Solution {
         int n=arr.length;
         List<Integer> ans=new ArrayList<>();
         HashMap<Integer,Integer> map=new HashMap<>();
-
         for(int ele:arr){
             // if(map.containsKey(ele)) map.put(ele,map.get(ele)+1);
             // else map.put(ele,1);
-            map.put(ele,map.getOrDefault(ele,0)+1) ;
+            map.put(ele,map.getOrDefault(ele,0)+1);
+            if(map.get(ele)==(n/3)+1) ans.add(ele);
         }
-        for(int key : map.keySet()){
-            if(map.get(key)>n/3) ans.add(key);
-        }
+        // for(int key : map.keySet()){
+        //    if(map.get(ele)==(n/3)+1) ans.add(ele);
+        // }
         return ans;
     }
-}
+} 
