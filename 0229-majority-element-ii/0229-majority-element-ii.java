8a364e4bafe8a63c3345a317fc83bc461c2d6571
RayHunter_1667 <karanbharvad1667@gmail.com>
2026-08-18 08:25:26 +0530

    Time: 926 ms (5.12%), Space: 52.5 MB (53.48%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -2,16 +2,33 @@ class Solution {
     public List<Integer> majorityElement(int[] arr) {
         int n=arr.length;
         List<Integer> ans=new ArrayList<>();
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int ele:arr){
-            // if(map.containsKey(ele)) map.put(ele,map.get(ele)+1);
-            // else map.put(ele,1);
-            map.put(ele,map.getOrDefault(ele,0)+1);
-            if(map.get(ele)==(n/3)+1) ans.add(ele);
+        for(int i=0;i<n;i++){
+            if(ans.size()==0||ans.get(0)!=arr[i]){
+                int count=0;
+                for(int j=0;j<n;j++){
+                    if(arr[i]==arr[j]) count++;
+                }
+                if(count>n/3) ans.add(arr[i]);
+            }
+            if(ans.size()==2) break;
         }
-        // for(int key : map.keySet()){
-        //    if(map.get(ele)==(n/3)+1) ans.add(ele);
-        // }
         return ans;
     }
 } 
+// class Solution {
+//     public List<Integer> majorityElement(int[] arr) {
+//         int n=arr.length;
+//         List<Integer> ans=new ArrayList<>();
+//         HashMap<Integer,Integer> map=new HashMap<>();
+//         for(int ele:arr){
+//             // if(map.containsKey(ele)) map.put(ele,map.get(ele)+1);
+//             // else map.put(ele,1);
+//             map.put(ele,map.getOrDefault(ele,0)+1);
+//             if(map.get(ele)==(n/3)+1) ans.add(ele);
+//         }
+//         // for(int key : map.keySet()){
+//         //    if(map.get(ele)==(n/3)+1) ans.add(ele);
+//         // }
+//         return ans;
+//     }
+// } 
