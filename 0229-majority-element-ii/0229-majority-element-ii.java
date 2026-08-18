f4064b6805ba1b4192a7c0f3f47921ec5d29dbde
RayHunter_1667 <karanbharvad1667@gmail.com>
2026-08-18 07:59:00 +0530

    Time: 14 ms (42.85%), Space: 53.4 MB (5.63%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -5,17 +5,12 @@ class Solution {
         HashMap<Integer,Integer> map=new HashMap<>();
 
         for(int ele:arr){
-            if(map.containsKey(ele)){
-                map.put(ele,map.get(ele)+1);
-            }else{
-                map.put(ele,1);
-            }
+            // if(map.containsKey(ele)) map.put(ele,map.get(ele)+1);
+            // else map.put(ele,1);
+            map.put(ele,map.getOrDefault(ele,0)+1) ;
         }
-
         for(int key : map.keySet()){
-            if(map.get(key)>n/3){
-                ans.add(key);
-            }
+            if(map.get(key)>n/3) ans.add(key);
         }
         return ans;
     }
