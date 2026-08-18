b89eec8739fcaf1e1a9b853b317b9e2051f6ed4c
RayHunter_1667 <karanbharvad1667@gmail.com>
2026-08-18 08:52:08 +0530

    Time: 1 ms (99.89%), Space: 55.4 MB (66.83%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,16 +1,41 @@
+// optimal tc=(n) space=(1) moous algoritham
 class Solution {
     public int majorityElement(int[] arr) {
-        HashMap<Integer,Integer> map=new HashMap<>();
         int n =arr.length;
+        int count=0;
+        int ele=0;
         for(int x:arr){
-            if(map.containsKey(x)) map.put(x,map.get(x)+1);
-            else map.put(x,1);
-
-            if(map.get(x)>n/2) return x;
+            if(count==0){
+                ele=x;
+                count=1;
+            }else if(x==ele){
+                count++;
+            }else{
+                count--;
+            }
         }
+        int cnt=0;
+        for(int y:arr){
+            if(ele==y) cnt++;
+        }
+        if(cnt>n/2) return ele;
         return -1;
     }
 }
+// // better tc=(n) space=(n)
+// class Solution {
+//     public int majorityElement(int[] arr) {
+//         HashMap<Integer,Integer> map=new HashMap<>();
+//         int n =arr.length;
+//         for(int x:arr){
+//             if(map.containsKey(x)) map.put(x,map.get(x)+1);
+//             else map.put(x,1);//map.put(x,map.getOrDefault(x,0)+1)
+
+//             if(map.get(x)>n/2) return x;
+//         }
+//         return -1;
+//     }
+// }
 
 
 //tc=O(n^2)
