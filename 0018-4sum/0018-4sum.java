140dfc4808359b471de4550be9fc059c154a0712
RayHunter_1667 <karanbharvad1667@gmail.com>
2026-08-19 10:11:53 +0530

    Time: 18 ms (89.98%), Space: 46 MB (21.78%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,40 +1,64 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] arr, int target) {
-        int n = arr.length;
-
-        HashSet<List<Integer>> set = new HashSet<>();
-
-        for (int i = 0; i < n - 2; i++) {
-            for (int j = i + 1; j < n - 1; j++) {
-
-                HashSet<Long> st = new HashSet<>();
-
-                for (int k = j + 1; k < n; k++) {
-
-                    long sum = (long) arr[i] + arr[j] + arr[k];
-                    long fourth = (long) target - sum;
-
-                    if (st.contains(fourth)) {
-
-                        List<Integer> temp = new ArrayList<>();
-
+       int n=arr.length;
+       Arrays.sort(arr);
+       List<List<Integer>> ans =new ArrayList<>();
+       for(int i=0;i<n-1;i++){
+            if(i>0 && arr[i]==arr[i-1]) continue;
+            for(int j=i+1;j<n;j++){
+               if(j!=i+1 && arr[j]==arr[j-1]) continue;
+                int k=j+1;
+                int l=n-1;
+                while(k<l){
+                    long sum=(long) arr[i]+arr[j]+arr[k]+arr[l];
+                    if(sum==target){
+                        List<Integer> temp=new ArrayList<>();
                         temp.add(arr[i]);
                         temp.add(arr[j]);
                         temp.add(arr[k]);
-                        temp.add((int) fourth);
-
-                        Collections.sort(temp);
-                        set.add(temp);
+                        temp.add(arr[l]);
+                        ans.add(temp);
+                        k++;
+                        l--;
+                        while(k<l && arr[k]==arr[k-1]) k++;
+                        while(k<l && arr[l]==arr[l+1]) l--;
                     }
-
-                    st.add((long) arr[k]);
+                    else if(sum<target) k++;
+                    else l--;
                 }
             }
         }
-
-        return new ArrayList<>(set);
+        return ans;
     }
 }
+
+// better
+// class Solution {
+//     public List<List<Integer>> fourSum(int[] arr, int target) {
+//         int n = arr.length;
+//         HashSet<List<Integer>> set = new HashSet<>();
+//         for (int i = 0; i < n - 2; i++) {
+//             for (int j = i + 1; j < n - 1; j++) {
+//                 HashSet<Long> st = new HashSet<>();
+//                 for (int k = j + 1; k < n; k++) {
+//                     long sum = (long) arr[i] + arr[j] + arr[k];
+//                     long fourth = (long) target - sum;
+//                     if (st.contains(fourth)) {
+//                         List<Integer> temp = new ArrayList<>();
+//                         temp.add(arr[i]);
+//                         temp.add(arr[j]);
+//                         temp.add(arr[k]);
+//                         temp.add((int) fourth);
+//                         Collections.sort(temp);
+//                         set.add(temp);
+//                     }
+//                     st.add((long) arr[k]);
+//                 }
+//             }
+//         }
+//         return new ArrayList<>(set);
+//     }
+// }
 
 // brute force 
 // class Solution {
