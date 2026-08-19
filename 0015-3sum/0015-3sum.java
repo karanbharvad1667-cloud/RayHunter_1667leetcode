0590f717f8cc814780c58a5a16f639d67879e9d5
RayHunter_1667 <karanbharvad1667@gmail.com>
2026-08-19 08:50:22 +0530

    Time: 28 ms (96.85%), Space: 60.6 MB (5.49%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,27 +1,61 @@
+// optimal solution 
 class Solution {
     public List<List<Integer>> threeSum(int[] arr) {
         int n=arr.length;
+        Arrays.sort(arr);
         List<List<Integer>> ans=new ArrayList<>();
-        HashSet<List<Integer>> set =new HashSet<>();
         for(int i=0;i<n;i++){
-            HashSet<Integer> st=new HashSet<>();
-            for(int j=i+1;j<n;j++){
-                int third=-(arr[i]+arr[j]);
-                if(st.contains(third)){
-                    List<Integer> temp=new ArrayList<>();
-                    temp.add(arr[i]);
-                    temp.add(third);
-                    temp.add(arr[j]);
-                    temp.sort(null);
-                    set.add(temp);
-                }
-                st.add(arr[j]);
+                int j=i+1;
+                int k=n-1;
+                if(i>0 && arr[i]==arr[i-1]) continue;
+        while(j<k){
+            int sum=arr[i]+arr[j]+arr[k];
+            if(sum<0){
+                j++;
+            }else if(sum>0){
+                k--;
+            }else{
+                List<Integer> temp=new ArrayList<>();
+                temp.add(arr[i]);
+                temp.add(arr[j]);
+                temp.add(arr[k]);
+                ans.add(temp);
+                j++;
+                k--;
+                while(j<k && arr[j]==arr[j-1]) j++;
+                while(j<k && arr[k]==arr[k+1]) k--;
             }
         }
-        ans.addAll(set);
+        }
         return ans;
     }
 }
+// // better solution 
+// class Solution {
+//     public List<List<Integer>> threeSum(int[] arr) {
+//         int n=arr.length;
+//         List<List<Integer>> ans=new ArrayList<>();
+//         HashSet<List<Integer>> set =new HashSet<>();
+//         for(int i=0;i<n;i++){
+//             HashSet<Integer> st=new HashSet<>();
+//             for(int j=i+1;j<n;j++){
+//                 int third=-(arr[i]+arr[j]);
+//                 if(st.contains(third)){
+//                     List<Integer> temp=new ArrayList<>();
+//                     temp.add(arr[i]);
+//                     temp.add(third);
+//                     temp.add(arr[j]);
+//                     temp.sort(null);
+//                     set.add(temp);
+//                 }
+//                 st.add(arr[j]);
+//             }
+//         }
+//         ans.addAll(set);
+//         return ans;
+//     }
+// }
+
 // // brute force tc=(n^3 * log(no. of unique triplet))
 //  // space=2*O(no of triplet)
 // class Solution {
